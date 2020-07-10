51d06dd30b3c20daef186c46d98e7d2c03e28740
MazeArt <fcoarturo.marshall@gmail.com>
2020-07-10 15:59:41 -0400

    v0.5

--- tablero.js
+++ tablero.js
@@ -13,13 +13,44 @@ let Player =function(name,turn,points){
 }
 let currentGame
 const mimica_div = document.getElementById("mimica_");
-let movie //this is the selected random movie
-let numPlayers
+const twotruths_div = document.getElementById("twoTruths_");
+const palabras_div =  document.getElementById("palabras_");
+const quiensoy_div =  document.getElementById("quiensoy_");
+const trivia_div =  document.getElementById("trivia_");
+
+
+const numPlayers = sessionStorage.getItem("numPlayers");
 let playerTurn=0
+let timer
 
-mimica_div.style.display = "none"
+let movie //this is the selected random movie
+let personaje 
+
+let playerList= []
+
+function checkPlayerlist(){
+    for(var i=0;i<numPlayers;i++){
+        playerList[i]=sessionStorage.getItem("player"+i)
+
+    } 
+
+}
 
 
+
+//hides all game divs
+function hide_divs(){
+    mimica_div.style.display = "none"
+    palabras_div.style.display = "none"
+    twotruths_div.style.display = "none"
+    trivia_div.style.display = "none"
+    quiensoy_div.style.display = "none"
+}
+
+
+
+hide_divs()
+checkPlayerlist()
 
 //SETUP PAGE -- 
 // --Add Players
@@ -38,6 +69,7 @@ function init() {
     console.log('init');
     turn=0
     console.log('a new player was created: %s' ,  'tipo');
+    
 
    // player.innerHTML=gamePlayers[turn].name //change turn here
     isPlaying = false;
@@ -50,10 +82,12 @@ function rollDice(){
 }
 
 
+
+
 function getPlayerTurn(){
     console.log('its player %s turn' , playerTurn);
     //players in the Session
-    numPlayers = sessionStorage.getItem("numPlayers");
+    //numPlayers = sessionStorage.getItem("numPlayers");
     console.log("players::",sessionStorage.getItem("numPlayers"))
     //show player name in index
     player.innerHTML = sessionStorage.getItem("player"+playerTurn);
@@ -82,15 +116,31 @@ function getGame(){
     //make function to prevent repeating random
     lastGame=juego
 
-    mimica_div.style.display = "none"
+    hide_divs();
 
     switch(juego) {
         case 'Mímica':
           mimica();
           break;
+        
+        case 'Quien soy?':
+            quiensoy();
+        break;
+      
+        case 'Palabras Concatenadas':
+            palabrasConc();
+        break;  
+        
         case 'Trivia':
-          // code block
-          break;
+            trivia();
+            // code block
+        break;    
+        
+        case 'Dos Verdades y una Mentira':
+            // code block
+            twotruths();
+        break;  
+        
         default:
           // code block
       }
@@ -98,7 +148,7 @@ function getGame(){
     return juego;
 
 }
-
+/// Game Modules functions
 function getRandomFromList(list){
     var listlen=list.length;
     var random = Math.floor((Math.random()*list.length));
@@ -117,6 +167,78 @@ function mimica(){
     movie=getRandomFromList(movies)     
 }
 
+function quiensoy(){
+    
+    if(currentGame=='Quien soy?'){
+        
+        if (quiensoy_div.style.display === "none") {
+            quiensoy_div.style.display = "block";
+        }    
+    }
+    personaje=getRandomFromList(personajes)     
+}
+
+function trivia(){
+    
+    if(currentGame=='Trivia'){
+        
+        if (trivia_div.style.display === "none") {
+            trivia_div.style.display = "block";
+        }    
+    }
+    
+    var random = Math.floor((Math.random()*trivia_questions.length));
+
+    document.getElementById('pregunta').innerHTML = trivia_questions[random][0]      
+}
+
+function twotruths(){
+    
+    if(currentGame=='Dos Verdades y una Mentira'){
+        
+        if (twotruths_div.style.display === "none") {
+            twotruths_div.style.display = "block";
+        }    
+    }
+    //personaje=getRandomFromList(personajes)     
+}
+
+function palabrasConc(){
+    
+    if(currentGame=='Palabras Concatenadas'){
+        
+        if (palabras_div.style.display === "none") {
+            palabras_div.style.display = "block";
+        }    
+    }
+    randomPlayer=getRandomFromList(playerList) 
+    document.getElementById('random_player').innerHTML = randomPlayer   
+}
+
+let dTimer
+function nextPlayerTimer(){
+    randomPlayer=getRandomFromList(playerList) 
+    document.getElementById('random_player').innerHTML = randomPlayer
+
+   clearInterval(dTimer);
+   var timeleft = 3;
+   document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+
+    dTimer = setInterval(function(){
+        if(timeleft <= 0){
+            clearInterval(dTimer);
+            document.getElementById("countdown").innerHTML = "Perdió !!";
+        } else {
+            document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+        }
+        timeleft -= 1;
+    }, 1000);
+    //palabrasConc();
+    
+    //clearInterval(dTimer);
+    }
+   
+
 //roullette
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -127,7 +249,7 @@ async function wheelEffect(){
     mimica_div.style.display = "none"
     var t=20;
     var interval=Math.random()*(-1+Math.round(Math.random()))
-    var duration=300+interval*(100)
+    var duration=20+interval*(100) //300 
     var lastItem
     console.log('this is t: ',t); 
     while(t<duration){
@@ -140,7 +262,8 @@ async function wheelEffect(){
             juego=getRandomFromList(juegos)
         }
         lastItem=juego
-
+        //debugging
+           //  juego='Quien soy?'
         gameDisplay.innerHTML=juego;
         ;
     }
@@ -149,23 +272,36 @@ async function wheelEffect(){
 }
 
 
-//key press listen
+//KEY Press listen
 document.body.addEventListener('keydown', function(e) {
    console.log('key pressed, ',currentGame)
+   if(e.keyCode==77){
     if(currentGame=='Mímica'){
-      if(e.keyCode==77){
         console.log('pressed "m" when mimica is visible')
         document.getElementById('movie').innerHTML = movie   
     }
+    if(currentGame=='Quien soy?'){
+        console.log('pressed "m" when quien soy is visible')
+        document.getElementById('personaje').innerHTML = personaje   
+    }
+    if(currentGame=='Trivia'){
+        console.log('pressed "m" when quien soy is visible')
+        document.getElementById('respuesta').innerHTML = "I DON'T KNOW!"       
+    }
+
     }
 });
 document.body.addEventListener('keyup', function(e) {
     console.log('key pressed, ',currentGame)
+    if(e.keyCode==77){
      if(currentGame=='Mímica'){
-       if(e.keyCode==77){
          console.log('pressed "m" when mimica is visible')
          document.getElementById('movie').innerHTML = ''  
      }
+     if(currentGame=='Quien soy?'){
+        console.log('pressed "m" when quien soy is visible')
+        document.getElementById('personaje').innerHTML = ''  
+    }
      }
  });
 
@@ -176,19 +312,20 @@ document.body.addEventListener('keyup', function(e) {
 const juegos = [
     'Trivia',
     'Mímica',
-    'Palabras Prohibidas'
+    'Palabras Concatenadas',
+    'Dos Verdades y una Mentira',
+    'Quien soy?'
 ];
 
 
-const movies = [
-    'Perros de la Calle',
-    'Indiana Jones',
-    'Transformers',
-    'La Delgada Línea Roja',
-    'Guerra de las Galaxias',
-    'Inception ',
-    'Split',
-    'Titanic.',
+const movies = ['Perros de la Calle',
+'Indiana Jones',
+'Transformers',
+'La Delgada Línea Roja',
+'Guerra de las Galaxias',
+'Inception ',
+'Split',
+'Titanic.',
 'The Shawshank Redemption.',
 'Forrest Gump.',
 'American Beauty.',
@@ -307,4 +444,435 @@ const movies = [
 'Trading Places',
 'Scrooged'
 ];
+
+const personajes = ['Zheng He',
+'Yuri Gagarin',
+'Yoda',
+'Yasser Arafat',
+'Xenomorph',
+'Woody Woodpecker',
+'Woody (Toy Story)',
+'Woodrow Wilson',
+'Wolverine',
+'Wolfgang Amadeus Mozart',
+'Wizard of Oz',
+'Winston Churchill',
+'Winnie the Pooh',
+'William Wallace - Braveheart',
+'William Shakespeare',
+'Wilhelm Conrad RÃ¶ntgen',
+'Whitney Houston',
+'Wednesday Addams',
+'Walt Disney',
+'Wallace and Gromit',
+'Voltaire',
+'Voldemort',
+'Vladimir Putin',
+'Vladimir Lenin',
+'Vito Corleone (The Godfather)',
+'Virgil',
+'Vincent Vega- Pulp Fiction',
+'Vincent van Gogh',
+'Victor Hugo',
+'Vasco da Gama',
+'Valery Leontyev',
+'Umar',
+'Tyler Durden',
+'Travis Bickle',
+'Toshiro Mifune',
+'Tony Montana',
+'Tinker Bell',
+'Timur',
+'Tim Berners-Lee',
+'Thomas Jefferson',
+'Thomas Edison',
+'Thomas Aquinas',
+'Theodore Roosevelt',
+'The Terminator',
+'The Simpsons',
+'The Muppets',
+'The Munchkins',
+'The Mask',
+'The Joker',
+'Taylor Lautner',
+'Superman',
+'Steven Spielberg',
+'Steve Jobs',
+'Spock',
+'Spiderman',
+'Sophocles',
+'Socrates',
+'Snow White',
+'Snow White',
+'Snoopy',
+'Sloth (The Goonies)',
+'SimÃ³n BolÃ­var',
+'Silvio Berlusconi',
+'Sigmund Freud',
+'Shrek',
+'Shakira',
+'Selena Gomez',
+'Selena',
+'Scarlett Johansson',
+'Sarah Bernhardt',
+'Salvador DalÃ­',
+'Saladin',
+'Saint Peter',
+'Saddam Hussein',
+'Rugrats',
+'Rosa Luxemburg',
+'Ronald Reagan',
+'Roger Federer',
+'Roald Amundsen',
+'Richard Wagner',
+'Richard Nixon',
+'RenÃ© Descartes',
+'Rembrandt',
+'Raphael',
+'Rambo',
+'Rafael Correa',
+'Rabindranath Tagore',
+'Queen Victoria',
+'Qin Shi Huang',
+'Pythagoras',
+'Pyotr Ilyich Tchaikovsky',
+'Predator',
+'Pope John Paul II',
+'Pope Benedict XVI',
+'Plato',
+'Peter Paul Rubens',
+'Peter I of Russia',
+'Paul of Tarsus',
+'Pablo Picasso',
+'Pablo Neruda',
+'Otto von Bismarck',
+'Oscar Wilde',
+'Osama bin Laden',
+'Optimus Prime',
+'Octave Mirbeau',
+'Obi-Wan Kenobi',
+'Novak Djokovic',
+'Noam Chomsky',
+'Nikola Tesla',
+'Niels Henrik David Bohr',
+'Nicolaus Copernicus',
+'Nicolas Sarkozy',
+'NiccolÃ² Machiavelli',
+'Neo',
+'Nemo',
+'Nelson Mandela',
+'Neil Armstrong',
+'Nathalia Dill',
+'Napoleon Bonaparte',
+'Mustafa Kemal AtatÃ¼rk',
+'Muhammad ibn Musa al-Khwarizmi',
+'Muhammad',
+'Mufasa, the Lion King',
+'Muammar al-Gaddafi',
+'Mother Teresa',
+'Moses',
+'MoliÃ¨re',
+'Minnie Mouse',
+'Minions',
+'Mikhail Gorbachev',
+'Miguel de Cervantes',
+'Mickey Mouse',
+'Michelangelo',
+'Michael Schumacher',
+'Michael Jackson',
+'Michael Faraday',
+'Michael Corleone',
+'Maximus',
+'Max Planck',
+'Mary',
+'Martin Van Buren',
+'Martin Luther King, Jr.',
+'Martin Luther',
+'Marlon Brando',
+'Marlene Dietrich',
+'Mark Twain',
+'Marilyn Monroe',
+'Marie Curie',
+'Margaret Thatcher',
+'Marco Polo',
+'Marcel Proust',
+'Mao Zedong',
+'Mahmoud Ahmadinejad',
+'Mahatma Gandhi',
+'Madonna',
+'Mad Max',
+'Lyndon B. Johnson',
+'Ludwig van Beethoven',
+'Lu Xun',
+'Louis Pasteur',
+'Louis Armstrong',
+'Lionel Messi',
+'Linus Torvalds',
+'Lila Downs',
+'Li Bai',
+'Leonhard Euler',
+'Leonardo da Vinci',
+'Leon Trotsky',
+'Leo Tolstoy',
+'Lee',
+'Lech WaÅÄsa',
+'Leatherface',
+'Le Corbusier',
+'Laozi',
+'Lady Gaga',
+'Kofi Annan',
+'Kim Jong-il',
+'Kevin McCallister',
+'Kermit the Frog',
+'Katniss Everdeen',
+'Karl Marx',
+'Justin Bieber',
+'Julius Caesar',
+'Jules Verne',
+'Josh Duhamel',
+'Joseph Stalin',
+'Jorge Luis Borges',
+'John Tyler',
+'John Quincy Adams',
+'John McClane',
+'John Maynard Keynes',
+'John Locke',
+'John Lennon',
+'John F. Kennedy',
+'John Adams',
+'Johannes Kepler',
+'Johannes Gutenberg',
+'Johannes Brahms',
+'Johann Wolfgang von Goethe',
+'Johann Sebastian Bach',
+'Jimmy Wales',
+'Jimmy Carter',
+'Jigsaw',
+'Jesus Christ',
+'Jeremy Renner',
+'Jeanne d Arc',
+'Jean-Paul Sartre',
+'Jean-Jacques Rousseau',
+'Jean Auguste Dominique Ingres',
+'Jaws',
+'Jawaharlal Nehru',
+'Jason Bourne',
+'James Watt',
+'James Monroe',
+'James Madison',
+'James Joyce',
+'James Cook',
+'James Clerk Maxwell',
+'James Buchanan',
+'James Bond',
+'Jacques Chirac',
+'Jacques Cartier',
+'J. R. R. Tolkien',
+'J. K. Rowling',
+'Isaac Newton',
+'Iron Man',
+'Ingmar Bergman',
+'Indira Gandhi',
+'Indiana Jones',
+'Immanuel Kant',
+'Hugo ChÃ¡vez',
+'Huang Xian Fan',
+'Hosni Mubarak',
+'Homer',
+'Hokusai',
+'Hirohito',
+'Hippocrates',
+'Hillary Rodham Clinton',
+'Herodotus',
+'HernÃ¡n CortÃ©s',
+'Henry Ford',
+'Henri Matisse',
+'Hebe Camargo',
+'Harry S. Truman',
+'Harry Potter',
+'Hans Christian Andersen',
+'Hannibal Lecter',
+'Han Solo',
+'Hal 9000',
+'Gwyneth Paltrow',
+'Gottfried Wilhelm von Leibniz',
+'Gollum',
+'Godzilla',
+'Giuseppe Verdi',
+'Ghostface',
+'Gerald Ford',
+'George Washington',
+'George Orwell',
+'George H. W. Bush',
+'George Gordon Noel Byron, 6th Baron Byron',
+'George Frideric Handel',
+'George Bush',
+'George Bernard Shaw',
+'Georg Wilhelm Friedrich Hegel',
+'Geoffrey Chaucer',
+'Genghis Khan',
+'Gautama Buddha',
+'Gandalf',
+'Galileo Galilei',
+'Gabriel GarcÃ­a MÃ¡rquez',
+'Fyodor Dostoyevsky',
+'Frodo',
+'Friedrich Nietzsche',
+'Friedrich Engels',
+'Frida Kahlo',
+'Freddy Krueger',
+'Franz Schubert',
+'Franz Kafka',
+'Franklin D. Roosevelt',
+'Frankenstein',
+'Frank Lloyd Wright',
+'Francisco Goya',
+'Francesco Totti',
+'FranÃ§ois Hollande',
+'FrÃ©dÃ©ric Chopin',
+'Forrest Gump',
+'Fidel Castro',
+'Fernando Alonso',
+'Ferdinand Magellan',
+'Euclid',
+'Erwin SchrÃ¶dinger',
+'Ernest Rutherford',
+'Ernest Hemingway',
+'Enrico Fermi',
+'Elvis Presley',
+'Elizabeth II of the United Kingdom',
+'Elizabeth I of England',
+'Edmund Hillary',
+'Edgar Allan Poe',
+'E.T.',
+'Dwight D. Eisenhower',
+'Du Fu',
+'Dorothy (Wizard of Oz)',
+'Donald Duck',
+'Doc. Emmett Brown (Back to the Future)',
+'Dmitry Medvedev',
+'Dmitri Mendeleev',
+'Diego VelÃ¡zquez',
+'Derek Zoolander - Zoolander',
+'Darth Vader',
+'Dante Alighieri',
+'Corbin Bleu',
+'Constantine I',
+'Confucius',
+'Cleopatra VII of Egypt',
+'Claude Monet',
+'Christopher Columbus',
+'Chris Pine',
+'Che Guevara',
+'Charlie Chaplin',
+'Charles Dickens',
+'Charles de Gaulle',
+'Charles Darwin',
+'Charlemagne',
+'Carl Linnaeus',
+'Carl Friedrich Gauss',
+'Captain James T. Kirk',
+'Captain America',
+'C-3PO and R2-D2 (Droids)',
+'Bugs Bunny',
+'Britney Spears',
+'Boris Yeltsin',
+'Bob Marley',
+'Blaise Pascal',
+'Bill Gates',
+'Bill Clinton',
+'Big Bird',
+'Bertrand Russell',
+'Benito Mussolini',
+'Benazir Bhutto',
+'Basho',
+'Barack Obama',
+'Avril Lavigne',
+'Avicenna',
+'Augustus',
+'Augusto De Luca',
+'Augustine of Hippo',
+'Aristotle',
+'Ariel (Little Marmaid)',
+'Archimedes',
+'Antonio Vivaldi',
+'AntonÃ­n DvoÅÃ¡k',
+'Anton Chekhov',
+'Anthony Blair',
+'Angelina Jolie',
+'Angela Merkel',
+'Andy Warhol',
+'Andrew Jackson',
+'Andrew Garfield',
+'Anastacia',
+'Alvin and the Chipmunks',
+'Alfred Nobel',
+'Alfred Hitchcock',
+'Alexander the Great',
+'Alexander Graham Bell',
+'Aleksandr Solzhenitsyn',
+'Aleksandr Pushkin',
+'Albrecht DÃ¼rer',
+'Albert Einstein',
+'Albert Camus',
+'Alan Turing',
+'Akira Kurosawa',
+'Akbar',
+'Agent J (Men in Black)',
+'Agatha Christie',
+'Adolf Hitler',
+'Ãdith Piaf',
+'Adam Smith',
+'Abraham Lincoln',
+'Abraham'
+]
+
+const trivia_questions = [ ['In what year was the first ever Wimbledon Championship held?',' 1877.'],
+['Hg is the chemical symbol of which element?',' Mercury.'],
+['Which email service is owned by Microsoft?',' Hotmail.'],
+['Which country produces the most coffee in the world?',' Brazil.'],
+['In which city was Jim Morrison buried?',' Paris.'],
+['Which song by Luis Fonsi and Daddy Yankee has the most views (of all time) on YouTube?',' Despacito.'],
+['What was the first state?',' Delaware.'],
+['What is the capital city of Spain?',' Madrid.'],
+['What is the painting ‘La Gioconda’ more usually known as?',' The Mona Lisa.'],
+['What is Chandler’s last name in the sitcom Friends? ',' Bing.'],
+['About how many taste buds does the average human tongue have? ',' 10, 000.'],
+['Who did Madonna kiss at the 2003 VMAs? ',' Britney Spears and Christina Aguilera.'],
+['How much does the Chewbacca costume weigh? ',' Eight pounds.'],
+['What colors are the Norwegian flag? ',' Red, white, and blue.'],
+['Where would you find the world’s most ancient forest? ',' Daintree Forest north of Cairns, Australia.'],
+['Globe and Jerusalem are types of what? ',' Artichoke.'],
+['Which is the highest waterfall in the world? ',' Angel Falls, Venezuela.'],
+['Ludwig Van Beethoven was born in 1770 in which city? ',' Berlin.'],
+['What is the third sign of the Zodiac? ',' Gemini.'],
+['What is Ariana Grande’s brother’s name? ',' Frankie.'],
+['Who discovered Penicillin? ',' Fleming.'],
+['Name the three primary colors. ',' Red, yellow and blue.'],
+['Which name is rapper Sean Combs better known by? ',' P. Diddy'],
+['Which country invented tea? ',' China.'],
+['Pure water as a pH level of around? ',' 7.'],
+['Which is the only vowel on a standard keyboard that is not on the top line of letters? ',' A.'],
+['Who starts first in chess? ',' White.'],
+['What was Britney Spears’ first song? ',' Baby One More Time.'],
+['What did Queen Anne die of? ',' A stroke.'],
+['Groups of lions are known as what? ',' Prides'],
+['How many pairs of wings does a bee have? ',' Two.'],
+['What language has the most words? ',' English.'],
+['What’s the most expensive home in the world? ',' Buckingham Palace.'],
+['Kodiak island is in which US state? ',' Alaska.'],
+['Which castle is on the island of Anglesey? ',' Beaumaria.'],
+['Which reality show series is Andy Cohen’s favorite? ',' Real Housewives.'],
+['How long does it take to hard boil an egg? ',' 7 minutes.'],
+['What nationality was Marco Polo? ',' Venetian'],
+['Which scientist was awarded the 1921 Nobel Prize in Physics? ','  Albert Einstein.'],
+['Name the world’s largest ocean. ',' Pacific.'],
+['What did the Crocodile swallow in Peter Pan? ',' An alarm clock.'],
+['What state is the Lincoln family home (Hildene) located in? ',' Vermont.'],
+['Which actress said, “Fasten your seatbelts. It’s going to be a bumpy night,” in All About Eve? ',' Bette Davis (as Margo Channing.)'],
+['Zurich is the largest city in what country? ',' Switzerland.'],
+['How many phases of the moon are there? ',' Eight.'],
+['What’s the hardest rock? ',' A diamond.'],
+['How many bones do sharks have in their bodies? ',' None!']
+]
 
